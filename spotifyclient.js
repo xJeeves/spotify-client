a01703c2aeb4c25b2c2a6574c58a9a38634d7781
xJeeves <54879161+xJeeves@users.noreply.github.com>
2019-12-27 19:02:34 -0800

    Update spotifyclient.js
    very importanttt

--- spotifyclient.js
+++ spotifyclient.js
@@ -3,7 +3,8 @@ const path  = require('path');
 const url = require('url');
 
 let win;
-app.commandLine.appendSwitch('widevine-cdm-path', '/path/to/widevine_library')
+// DO NOT FORGETTT TOO ADD WIDEVINE PLUGIN TO YOUR CODEEEEEEEEEEEEEEEEREEEEE SPOTIFY WILL DENY PLAYBACK WITHOUT ITTTTTTTT
+app.commandLine.appendSwitch('widevine-cdm-path', 'Program Files(x86)/Google/Chrome/Application/CHROME_VERSION/WidevineCdm/_platform_specific/win_(x86|x64)/')
 // The version of plugin can be got from `chrome://components` page in Chrome.
 app.commandLine.appendSwitch('widevine-cdm-version', '1.4.8.866')
 function createWindow(){
